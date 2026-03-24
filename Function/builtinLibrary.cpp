af17e8e87194648cc79679a10b7525800fdb167d
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-03-24 20:24:57 +0530

    builtin

---
+++ Function/builtinLibrary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+
+int maximumOfNumber(int x, int y){
+    return max(x,y) ;
+     
+}
+
+int main(){
+    cout << maximumOfNumber(3,4)<< endl ;
+    cout << sqrt(44) ;
+    
+}
