d69fe7b6fc985428f4700d5d47548870ebcb1a6d
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-17 13:43:04 +0530

    rotation of array

--- Array/Lecture2/VECTOR/reverseByPartwise.cpp
+++ Array/Lecture2/VECTOR/reverseByPartwise.cpp
@@ -1,7 +1,7 @@
-// write programme to reverse an array without using extra array(using two pointer)
+// write programme to reverse an array without using extra array by part wise
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 void display(vector<int> &a)
@@ -24,9 +24,10 @@ int main()
 
     display(v);
 
+    // we have to reverse only index 0 to 3(i=0 and j=3)
     // M1(using while loop)
     int i = 0;
-    int j = v.size() - 1;
+    int j = 3;
     while (i <= j)
     {
         int temp = v[i];
@@ -38,15 +39,12 @@ int main()
     display(v);
 
     // M2(using for loop)
-    for (int i = 0, j = v.size() - 1; i <= j; i++, j--)
+    for (int i = 0, j = 3 ; i <= j; i++, j--)
     {
         int temp = v[i];
         v[i] = v[j];
         v[j] = temp;
     }
     display(v);
-
-    // M3 by buildin function  (for this we have to include <algorithm>)
-    reverse(v.begin(), v.end());
-    display(v);
+   
 }
---
+++ Array/Lecture2/VECTOR/rotateArray.cpp
@@ -0,0 +1,48 @@
+// write programme to reverse an array without using extra array by part wise
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void display(vector<int> &a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void displaypart(int i, int j , vector<int>&a){
+    while (i <= j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+    }
+}
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    v.push_back(60);
+    v.push_back(70);
+    v.push_back(80);
+
+    int k = 50;  // rotate k times
+    int n = v.size();
+    if(k>n) k = k%n ;
+    display(v);
+    displaypart(0,n-k-1,v);
+    displaypart(n-k,n-1,v);
+    displaypart(0,n-1,v);
+    display(v);
+  
+   
+}
