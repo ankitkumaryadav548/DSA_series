724450421a74bb73a56994c25052f37446ea50b6
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-22 17:23:16 +0530

    maximum value from string

---
+++ String/Lecture2/maxInStringarray.cpp
@@ -0,0 +1,28 @@
+//return index of string which has maximum value
+
+//steps to solve
+//1. convert string to integer
+//2. then find maximum element from the integer array
+//3. at last print which string is maximum
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int main(){
+    string s[] = {"123","00234","34","00076"};
+    int max = stoi(s[0]);
+    string maxS = s[0];
+    for(int i=0;i<4;i++){
+        int n = stoi(s[i]);
+        if(n>max){
+            max = n;
+            maxS = s[i];
+        }
+    }
+    cout << max << endl;
+    cout << maxS << endl;
+
+
+}
