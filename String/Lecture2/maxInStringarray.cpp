22c0118a0ba9a88f60f8b01f6652d26dc2878fae
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-23 21:45:38 +0530

    update

--- String/Lecture2/maxInStringarray.cpp
+++ String/Lecture2/maxInStringarray.cpp
@@ -21,6 +21,24 @@ int main(){
     }
     cout << max << endl;
     cout << maxS << endl;
-
 }
 
+//practise 1
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s[] = {"865","8610","007564","4011","000000654"};
+    int max = stoi(s[0]);
+    string maxS = s[0];
+    for(int i=0;i<5;i++){
+        int n = stoi(s[i]);
+        if(n>max) {
+            max = n ;
+            maxS = s[i];
+        }
+    } 
+    cout << max << endl;
+    cout << maxS << endl;
+}
