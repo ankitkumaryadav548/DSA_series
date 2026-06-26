8e92e2ba94bd9998fbdecee70acd05964a392a0f
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-26 20:28:38 +0530

    Method to solve question

--- timeANDspaceComplexity/basicQuestion.cpp
+++ timeANDspaceComplexity/basicQuestion.cpp
@@ -2,11 +2,16 @@
 
 
 
+// steps for solution
+//M1 1stly we have to find sum of numbers
+//M2 2ndly we have to find sum of series using formula n*(n+1)/2 
+//M3 3rdly we have to find subtract then we will get duplicate number
+
 //M3 (most optimal solution)
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1,2,3,4,4,5,6,7};
+    int arr[] = {1,2,3,4,1,5,6,7};
     int n = sizeof(arr)/sizeof(arr[0])-1;
     int sum = 0;
     for(int i=0;i<=n;i++){
