317b1db28126d86c5c6301cd4d2e18941cc37509
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-28 21:57:56 +0530

    update

--- String/Lecture1/builtInfunction.cpp
+++ String/Lecture1/builtInfunction.cpp
@@ -5,48 +5,48 @@ using namespace std;
 int main(){
 
     //1.length()
-    // string str = "your are studing String ";
-    // cout << str.size(); // same
-    // cout << str.length(); // same
+    string str = "your are studing String ";
+    cout << str.size(); // same
+    cout << str.length(); // same
 
     //2.push_back
-    // string name = "Ankit";
-    // name.push_back('e'); // correct => only one character at a time 
-    // // name.push_back('esj'); // not correct => not multiple character
-    // cout << name << endl;
+    string name = "Ankit";
+    name.push_back('e'); // correct => only one character at a time 
+    // name.push_back('esj'); // not correct => not multiple character
+    cout << name << endl;
 
     //3.pop_back;
-    // name.pop_back();
-    // name.pop_back();
-    // cout <<name <<endl;
+    name.pop_back();
+    name.pop_back();
+    cout <<name <<endl;
 
     //4.add(+)
-    // string a = "abc";
-    // string b = "def";
-    // cout << a+b << endl;
-    // cout << b + "gsjd" << endl;
-    // cout << a + b + "jsdh" ;
+    string a = "abc";
+    string b = "def";
+    cout << a+b << endl;
+    cout << b + "gsjd" << endl;
+    cout << a + b + "jsdh" ;
 
 //     //5.reverse()
-    // string str = "ankit";
-    // cout << str << endl ;
-    // reverse(str.begin(),str.end()); 
-    // cout << str << endl ;
-    // reverse(str.begin()+2,str.end());
-    // cout << str << endl ;
-    // reverse(str.begin()+2,str.end()-1);
-    // cout << str << endl;
-    // reverse(str.begin()+2,str.begin()+5);
-    // cout << str << endl;
+    string str = "ankit";
+    cout << str << endl ;
+    reverse(str.begin(),str.end()); 
+    cout << str << endl ;
+    reverse(str.begin()+2,str.end());
+    cout << str << endl ;
+    reverse(str.begin()+2,str.end()-1);
+    cout << str << endl;
+    reverse(str.begin()+2,str.begin()+5);
+    cout << str << endl;
 
-//     //6.substring()
-    // string str = "ankit";
-    // cout <<  str.substr(2) << endl ;
-    // cout <<  str.substr(2,2) << endl ;
-    // cout <<  str.substr(1,4) << endl ; 
+    //6.substring()
+    string str = "ankit";
+    cout <<  str.substr(2) << endl ;
+    cout <<  str.substr(2,2) << endl ;
+    cout <<  str.substr(1,4) << endl ; 
 
 //     // 7.to_string()
-//     int x = 145435;
-//     string s = to_string(x);
-//     cout << s;
-// }
+    int x = 145435;
+    string s = to_string(x);
+    cout << s;
+}
