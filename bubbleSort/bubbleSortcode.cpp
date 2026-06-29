40013a02436ee7c499a6e28ff3d5a32d73fd2597
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-06-29 20:18:04 +0530

    most efficient bubble sort

--- bubbleSort/bubbleSortcode.cpp
+++ bubbleSort/bubbleSortcode.cpp
@@ -1,17 +1,20 @@
-//bubble sort
+//bubble sorting
 
 #include<iostream>
 using namespace std;
 int main(){
     int n = 6;
-    int arr[6] = {3,4,1,6,2,5};
+    int arr[6] = {3,4,1,6,-2,-5};
     for(int i=0;i<n;i++){
         cout << arr[i]<< " ";
     }
     cout << endl;
+    //bubble sort
     for(int i=0;i<=n-1;i++){
-        for(int j=0;j<=n-1;j++){
-            if(arr[j]>arr[j+1]){
+        //traverse
+        // for(int j=0;j<=n-1;j++){
+        for(int j=0;j<=n-1-i;j++){  // most efficient to include (n-1-i)
+            if(arr[j]>arr[j+1]){  //swap
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp ;
@@ -24,3 +27,6 @@ int main(){
         cout << arr[i]<< " ";
     }
 }
+
+//we are including i in (n-1-i) so that sorting become more efficient 
+// in including i => all duplicate/ reduncing sorting will be ignore
