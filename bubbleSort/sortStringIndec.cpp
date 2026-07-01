7093209be9b3341b09f33a804add630d94fc050b
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-01 21:26:44 +0530

    update

---
+++ bubbleSort/sortStringIndec.cpp
@@ -0,0 +1,36 @@
+//sort string in descending order
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "AJYZSDXBMZVY";
+    string str = "";
+    for(int i=0;i<s.length();i++){
+        if(s[i]>'X'){
+            str.push_back(s[i]);
+        }
+    }
+    // cout << str;
+    int n = str.length();
+
+    for(int i=0;i<=n-1;i++){
+        //traverse
+        // for(int j=0;j<=n-1;j++){
+        bool flag = true;
+        for(int j=0;j<=n-1-i;j++){  // most efficient to include (n-1-i)
+            if(str[j]>str[j+1]){  //swap
+                int temp = str[j];
+                str[j] = str[j+1];
+                str[j+1] = temp ;
+                flag = false;
+                // swap(arr[j],arr[j+1]);  //sortcut
+
+            }
+        }
+        if(flag==true){  // means swap didn't happen
+            break;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout << str[i]<< " ";
+    }
+}
