10c5a4025c74905a0ff93ce34ac0e1602c518d38
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-09 16:39:24 +0530

    update

--- sortingQuestion/kRange.cpp
+++ sortingQuestion/kRange.cpp
@@ -1,6 +1,7 @@
 //given an array , arr[] containing n integers , the task is to find an integer (say k) such that after replacing each and every index of an array by |ai-k| where i belong to [1 to n], result is  sorted array. If no such integer exits that satisfies above condition then return -1.Find the Range
 #include<iostream>
 #include<algorithm>
+#include<climits>
 using namespace std;
 
 int main()
@@ -8,8 +9,8 @@ int main()
     int arr[] = {5 , 3, 10 ,3};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    double Kmin = -1e18;
-    double Kmax = 1e18;
+    double Kmin = double(INT_MIN);
+    double Kmax = double(INT_MAX);
 
     bool possible = true;
 
