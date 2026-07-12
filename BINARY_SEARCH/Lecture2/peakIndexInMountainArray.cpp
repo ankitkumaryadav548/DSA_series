601504f3d01f4b15b4cdac326d5b6456efa72e45
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-12 08:01:48 +0530

    update

--- BINARY_SEARCH/Lecture2/peakIndexInMountainArray.cpp
+++ BINARY_SEARCH/Lecture2/peakIndexInMountainArray.cpp
@@ -4,19 +4,19 @@
 //1.find element which is greater than previous and next element
 // and the return it 
 //method 1 => time complexity =>0(n) 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int arr[] = {1,2,5,7,4,3,0};  
-//     int n = sizeof(arr)/sizeof(arr[0]);
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,5,7,4,3,0};  
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-//     for(int i=0;i<n;i++){
-//         if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-//             cout << i ;
-//             break;
-//         }
-//     }
-// }
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            cout << i ;
+            break;
+        }
+    }
+}
 
 //method 2 => time complexity => 0(logn);
 
