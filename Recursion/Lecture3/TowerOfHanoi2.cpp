c704761f8b95b95f3c99a2fe3c14ff0c5e946f90
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-25 22:14:19 +0530

    tower of Hanoi

--- Recursion/Lecture3/TowerOfHanoi2.cpp
+++ Recursion/Lecture3/TowerOfHanoi2.cpp
@@ -31,3 +31,23 @@
 //     Hanoi(n,'a','b','c');
 // }
 
+//practise
+
+#include<iostream>
+using namespace std;
+
+void Hanoi(int n , char A, char B, char C){
+    if(n==0) return ;
+    Hanoi(n-1,A,C,B);
+    cout << A << "->" << C << endl;
+    Hanoi(n-1,B,A,C);
+
+}
+
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    Hanoi(n,'A','B','C');
+}
+
