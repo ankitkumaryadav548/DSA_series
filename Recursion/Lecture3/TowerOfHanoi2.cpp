360df6f8313a8137266919c7f33fac2c9872459b
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-26 21:33:08 +0530

    practise of tower of hanoi

--- Recursion/Lecture3/TowerOfHanoi2.cpp
+++ Recursion/Lecture3/TowerOfHanoi2.cpp
@@ -1,37 +1,37 @@
-// #include <iostream>
-// using namespace std;
-// void Hanoi(int n, char A , char B , char C){
-//     if (n==0) return ;
-//     Hanoi(n-1,A,C,B);
-//     cout << A <<"->" << C << endl;
-//     Hanoi(n-1,B,A,C);
-// }
-// int main(){
-//     int n;
-//     cout<<"Enter a Number: ";
-//     cin >> n;
-//     Hanoi(n,'A','B','C');
+#include <iostream>
+using namespace std;
+void Hanoi(int n, char A , char B , char C){
+    if (n==0) return ;
+    Hanoi(n-1,A,C,B);
+    cout << A <<"->" << C << endl;
+    Hanoi(n-1,B,A,C);
+}
+int main(){
+    int n;
+    cout<<"Enter a Number: ";
+    cin >> n;
+    Hanoi(n,'A','B','C');
 
-// }
+}
 
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// void Hanoi(int n,char A,char B,char C){
-//     if(n==0) return ;
-//     Hanoi(n-1,A,C,B);
-//     cout << A << "->" << C << endl;
-//     Hanoi(n-1,B,A,C);
-// }
-// int main(){
-//     int n;
-//     cout << "Enter a Number: ";
-//     cin >> n;
-//     Hanoi(n,'a','b','c');
-// }
+void Hanoi(int n,char A,char B,char C){
+    if(n==0) return ;
+    Hanoi(n-1,A,C,B);
+    cout << A << "->" << C << endl;
+    Hanoi(n-1,B,A,C);
+}
+int main(){
+    int n;
+    cout << "Enter a Number: ";
+    cin >> n;
+    Hanoi(n,'a','b','c');
+}
 
-//practise
+// //practise
 
 #include<iostream>
 using namespace std;
@@ -50,4 +50,22 @@ int main(){
     cin >> n;
     Hanoi(n,'A','B','C');
 }
+
+//practise tower of Hanoi
+#include <iostream>
+using namespace std;
+
+void hanoi(int n, char a, char b, char c){
+    if(n==0) return;
+    hanoi(n-1,a,c,b);
+    cout << a << "->" << c << endl;
+    hanoi(n-1,b,a,c);
+}
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    hanoi(n, 'a','b','c');
+}
+
 
--- Recursion/Lecture3/printArray.cpp
+++ Recursion/Lecture3/printArray.cpp
@@ -1,30 +1,52 @@
-//printing all element of array using recursion
+// //printing all element of array using recursion
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// void display(int arr[],int n,int idx){
+//     if(idx == n) return ;
+//     cout <<  arr[idx] << " ";
+//     display(arr,n,idx+1);
+// }
+
+// void display2(vector<int>&v,int idx){
+//     if(idx == v.size()) return ;
+//     cout <<  v[idx] << " ";
+//     display2(v,idx+1);
+// }
+// int main(){
+//     int arr[] = {23,34,4,23,46,6,89,45};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     display(arr,n,0);
+//     cout << endl;
+        
+//     //using vector
+//     vector<int> v(n);
+//     for(int i=0;i<n;i++){
+//         v[i] = arr[i];
+//     }
+
+//     display2(v,0);
+
+// }
+
+
+// // practise to print array using recursion
+// //  23 34 45 78 5 31 
 
 #include<iostream>
-#include<vector>
 using namespace std;
 
-void display(int arr[],int n,int idx){
+void display(int arr[], int n, int idx){
     if(idx == n) return ;
-    cout <<  arr[idx] << " ";
+    cout << arr[idx] << " ";
     display(arr,n,idx+1);
-}
-void display2(vector<int>&v,int idx){
-    if(idx == v.size()) return ;
-    cout <<  v[idx] << " ";
-    display2(v,idx+1);
+
+
 }
 int main(){
-    int arr[] = {23,34,4,23,46,6,89,45};
+    int arr[] = {12, 34, 456, 67, 23, 46};
     int n = sizeof(arr)/sizeof(arr[0]);
     display(arr,n,0);
-    cout << endl;
-    //using vector
-    vector<int> v(n);
-    for(int i=0;i<n;i++){
-        v[i] = arr[i];
-    }
-
-    display2(v,0);
-
 }
