8bb592b9cad5f46c9857aef62724e286b348d415
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-29 15:55:55 +0530

    Prefix sum

--- Recursion/Lecture3/maxValueInArray.cpp
+++ Recursion/Lecture3/maxValueInArray.cpp
@@ -14,16 +14,16 @@ void maxValue(int arr[], int n, int idx,int max){   //printing max value
     maxValue(arr,n,idx+1,max);
 }
 
-void printMax(int arr[], int n, int idx){   //storing max value
-    if(idx==n) return INT_MIN;
-    return max(arr[idx],printMax(arr,n,idx+1));
+// void printMax(int arr[], int n, int idx){   //storing max value
+//     if(idx==n) return INT_MIN;
+//     return max(arr[idx],printMax(arr,n,idx+1));
 
-}
+// }
 int main(){
     int INT_MIN;
     int arr[] = {12, 34, 456, 67, 23, 46};
     int n = sizeof(arr)/sizeof(arr[0]);
     maxValue(arr,n,0,INT_MIN);
     cout << endl;
-    printMax(arr,n,0);
+    // printMax(arr,n,0);
 }
---
+++ SPECIAL_ALGORITHM_ARRAY/PREFIX_SUM/prefixSum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5,5,10};  //o/p => {1,3,6,10,15,21}
+    int n  = sizeof(arr)/sizeof(arr[0]);
+    //formation of prefix sum
+    for(int i=1;i<n;i++){
+        arr[i] = arr[i] + arr[i-1];
+    }
+
+    //check if x exists or not
+    int idx = -1;
+    for(int i=1;i<n;i++){
+        if(2*arr[i]==arr[n-1]){
+            idx = i;
+            break;
+        } 
+    }
+    if(idx!=-1) cout << "Yes it is partitioned " << idx;
+    else cout << "Cannot partitioned";
+}
