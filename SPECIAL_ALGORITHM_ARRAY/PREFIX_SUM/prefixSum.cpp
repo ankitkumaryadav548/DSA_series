160aacb901bfa00dac801a37a6f747c853eee117
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-07-29 16:02:27 +0530

    update prefix sum

--- SPECIAL_ALGORITHM_ARRAY/PREFIX_SUM/prefixSum.cpp
+++ SPECIAL_ALGORITHM_ARRAY/PREFIX_SUM/prefixSum.cpp
@@ -1,7 +1,7 @@
 //partitionation means breaking array into two equal part => sum of both partitoned array must be equal 
 
 
-//formula to solve this question 
+//formula to solve this question
 // 0 to x == x+1 to n-1
 // pre[x] = pre[n-1] -pre[x+1-1]
 // 2*pre[x] = pre[n-1]
