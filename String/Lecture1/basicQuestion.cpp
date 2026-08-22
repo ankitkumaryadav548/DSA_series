9271234bd736f4a87feaac63f1ca6b26f69b4c1d
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-08-22 19:31:17 +0530

    string revision

--- String/Lecture1/basicQuestion.cpp
+++ String/Lecture1/basicQuestion.cpp
@@ -1,13 +1,16 @@
-//input a string of length n and count all the vowel in the given string
+// input a string of length n and count all the vowel in the given string
 #include<iostream>
 #include<string>
-using namespace std;
+using namespace std ;  
 
 int main() {
     string str;
 
     cout << "Enter a string: ";
-    getline(cin, str);
+getline(cin, str);     // it will take input to whole text including space when we
+                        // press enter then it will stop taking input
+
+
 
     int count = 0;
 
@@ -24,4 +27,30 @@ int main() {
 
     return 0;
 }
+
+// Following is wrong code 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+        string str = "ankitkumaryadav";
+        int n = str.size();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(str[i]== 'a'||'e'||'i'||'o'||'u')
+            count ++ ;
+        }
+        cout << "no. of vowels are: " << count ;
+        return 0;
+}
+
+// str[i]== 'a'||'e'||'i'||'o'||'u' => this codition is wrong 
+// why our condition does't work because 
+// C++ does not automatically compare str[i] with every character.
+// C++ interprets it roughly as:
+// (str[i] == 'a') || ('e') || ('i')
+// Characters like 'e' and 'i' have non-zero ASCII values, so they are treated as true.
+// Therefore, your condition effectively becomes always true, and count gets incremented for every character and count will count all character 
 
