c20b4cc9e16cbe05f1aff8de876e172e0c16e1c8
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-08-26 19:15:27 +0530

    question practise on binary search

--- BINARY_SEARCH/Lecture1/firstOccurance.cpp
+++ BINARY_SEARCH/Lecture1/firstOccurance.cpp
@@ -1,11 +1,11 @@
-//Given a sorted an array of n element and a target 'x'.Find the first occurance of 'x' in the array, if element is not present then return -1.
+//Given a sorted an array of n element and a target 'x'.Return index of the first occurance of 'x' in the array, if element is not present then return -1.
 
 //Method 1 => time complexity = 0(n)
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1,2,2,5,6,3,3,3,3};
-    int n = 9;
+    int arr[] = {1,2,2,2,3,3,3,3,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
     int x = 3;
     for(int i=0;i<n;i++){
         if(arr[i]==x){
@@ -20,8 +20,8 @@ int main(){
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1,2,2,2,5,6,3,3,3,3};
-    int n = 10;
+    int arr[] = {1,2,2,2,3,3,3,3,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
     int x = 3;
 
     int low = 0;
