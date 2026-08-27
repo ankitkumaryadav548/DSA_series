80c22d3fe77c1b5f7ee454415a7904b49af3eb86
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-08-27 17:39:57 +0530

    mergeSort Algorithm

---
+++ SORTING/MergeSort/mergeSort.cpp
@@ -0,0 +1,159 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// void merge(vector<int>&a , vector<int>&b , vector<int>&res){
+//     int i = 0;  //a
+//     int j = 0;  //b
+//     int k = 0;  //res
+
+//     while(i<a.size()  && j<b.size()){
+//         if(a[i]<b[j]){
+//             res[k] = a[i];
+//             i++ ;
+//             k++;
+//         }
+//         else { // a[i]>b[j]
+//             res[k] = b[j];
+//                 j++ ;
+//                 k++ ;
+//         }
+//         if(i==a.size()){  //a is at end ;
+//             while(j<b.size()){
+//                 res[k++] = b[j++];
+//                 // k++ ;
+//                 // j++;
+//             }
+//         }
+//         if(j==b.size()){ // b is at end ;
+//             while(i<a.size()){
+//                 res[k++] = a[i++];
+//                 // k++ ;
+//                 // i++;
+//             }
+//         }
+//     }
+// }
+
+// void mergeSort(vector<int>&v){
+//     int n = v.size();    // 6, 4, 2, 7, 8, 3, 1
+//     if(n==1) return ;
+//     int n1 = n/2;        // n1 = 3
+//     int n2 = n - n/2 ;   // n2 = 4
+//     vector<int> a(n1) , b(n2) ;
+//     //copy paste
+//     for(int i = 0;i<n1;i++){
+//         a[i] = v[i];
+//     }
+//     for(int i = 0;i<n2;i++){
+//         b[i] = v[n1+i];
+//     }
+
+//     //magic
+//     mergeSort(a);
+//     mergeSort(b);
+
+//     //merge
+//     merge(a,b,v);
+
+
+// }
+
+// int main(){
+//     int arr[] = {6, 4, 2, 7, 8, 3, 1};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     vector<int> v(arr , arr+n);
+//     for(int i=0; i<n;i++){ // printing vector
+//         cout << v[i] << " ";
+//     }
+//     cout << endl;
+//     mergeSort(v);  //calling funcion 
+
+//     for(int i=0; i<n;i++){  //again printing 
+//         cout << v[i] << " ";
+//     }
+// }
+
+
+//practise of merge sort
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int> &a , vector<int> &b , vector<int> &res){
+    int i = 0; // a
+    int j = 0; // b
+    int k = 0; // c
+    // int n1 = sizeof(a);
+    // int n2 = sizeof(b);
+    while(i<a.size() && j < b.size()){
+        if(a[i]<b[j]){
+            res[k] = a[i] ;
+                i++ ;
+                k++;
+            
+        }
+        else{  // if(a[i]>b[j])
+                res[k] = b[j];
+                j++ ;
+                k++ ;
+        }
+    }
+
+        if(i==a.size()){
+            while(j <b.size()){
+                res[k] = b[j];
+                j++ ;
+                k++ ; 
+            }
+        }
+        if(j==b.size()){
+            while(i <a.size()){
+                res[k] = a[i];
+                i++ ;
+                k++ ; 
+            }
+        }
+    
+
+}
+void mergeSort(vector<int>&v){
+    int n = v.size();
+    if(n==1) return ;
+    int n1 = n/2 ;
+    int n2 = n - n/2;
+    vector<int> a(n1),b(n2);
+    for(int i=0;i<n1;i++){
+        a[i] = v[i];  // half part is stored in a[i]
+    }
+    for(int i=0;i<n2;i++){
+        b[i] = v[n1+i];  // next half part is stored in b[i]
+    }
+
+    // magic
+    mergeSort(a);
+    mergeSort(b);
+
+    // merging two sorted array
+    merge(a,b,v);
+
+    
+}
+
+int main(){
+    int arr[] = {4,2,9,7,1,3,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int>v(arr , arr+n);
+    for(int i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    mergeSort(v) ;
+
+    for(int i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
+}
+
+
--- SORTING/selectionANDinsertionSort/insertionSort.cpp
+++ SORTING/selectionANDinsertionSort/insertionSort.cpp
@@ -1,42 +1,83 @@
-//insertion sort
-//insertion sorting is the stable sorting algorithm
+// //insertion sort
+// //insertion sorting is the stable sorting algorithm
 
-//steps for selection sort =>
-// Assume the first element is already sorted.
-// Pick the next element (key).
-// Compare the key with elements on its left.
-// Shift all larger elements one position to the right.
-// Insert the key into its correct position.
-// Repeat until all elements are sorted.
+// //steps for selection sort =>
+// // Assume the first element is already sorted.
+// // Pick the next element (key).
+// // Compare the key with elements on its left.
+// // Shift all larger elements one position to the right.
+// // Insert the key into its correct position.
+// // Repeat until all elements are sorted.
 
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n = 5;
+//     int arr[n] = {5,4,1,3,2};
+//     for(int i=0;i<n;i++){
+//         cout << arr[i] << " ";
+//     } 
+//     cout << endl;
+//     for(int i=1;i<n;i++){
+//         int j = i;
+//         // while(j>=1){    //M1
+//         //     if(arr[j]<arr[j-1]) swap(arr[j],arr[j-1]);
+//         //     else{
+//         //         // (arr[j]>=arr[j-1]) ;
+//         //         break; 
+//         //     }
+//         //     j-- ;1
+//         // }
+//         //M2
+//         while(j>=1 && arr[j-1]> arr[j]){
+//             swap(arr[j],arr[j-1]);
+//             j-- ;
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         cout << arr[i] << " ";
+//     }
+// }
+
+
+
+#include <iostream>
 using namespace std;
+
 int main(){
-    int n = 5;
-    int arr[n] = {5,4,1,3,2};
+    // int arr[] = {3,5,7,2,1,6,9} ;
+    // int n = sizeof(arr)/sizeof(arr[0]);
+
+    int arr[5];
+    int n =5;
+    cout << "Enter element : ";
     for(int i=0;i<n;i++){
-        cout << arr[i] << " ";
-    } 
+        cin >> arr[i] ;
+    }
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " " ;
+    }
     cout << endl;
-    for(int i=1;i<n;i++){
+    
+
+    for(int i = 1;i<n-1;i++){
         int j = i;
-        // while(j>=1){    //M1
-        //     if(arr[j]<arr[j-1]) swap(arr[j],arr[j-1]);
-        //     else{
-        //         // (arr[j]>=arr[j-1]) ;
-        //         break; 
-        //     }
-        //     j-- ;1
-        // }
-        //M2
-        while(j>=1 && arr[j-1]> arr[j]){
-            swap(arr[j],arr[j-1]);
-            j-- ;
-        }
+        while(j>=1 ){
+            if(arr[j]<arr[j-1]){
+                swap(arr[j],arr[j-1]);
+                j-- ;
+            }
+            else break;
+        }        
+        
     }
     for(int i=0;i<n;i++){
-        cout << arr[i] << " ";
+        cout << arr[i] << " " ;
     }
+
 }
+
+// insertion sorting is known as Inplace sorting algorithm
 
