ec7ad45c50fe5e0b10c1c856627240b4f06f0770
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-09-02 19:15:03 +0530

    insertion at head , insertion at end

--- LinkedList/Lecture1/linkedlist.cpp
+++ LinkedList/Lecture1/linkedlist.cpp
@@ -12,12 +12,24 @@ class Node{
     }
 };
 
-void display(Node* head){
-    Node* temp = head;
-    while(temp != NULL){
+void displayRec(Node* head){   // T.C => 0(n)
+    if(head == NULL) return ; // S.C => 0(n)
+    cout << head->val << " " ;
+    displayRec(head->next);
+}
+
+void display(Node* head){  //T.C => 0(n)
+    Node* temp = head;     // S.C => 0(1)
+    while(temp != NULL){   //iterative displaying of element is best then recursive
         cout << temp->val <<" ";
         temp = temp->next ;
     }
+}
+
+void displayReverse(Node* head){   // function call 
+    if(head == NULL) return ; // base case
+    displayReverse(head->next);  //call
+    cout << head->val << " " ; //laam 
 }
 
 int size(Node* head){
@@ -38,6 +50,7 @@ int sum(Node* head){
     }
     return s;
 }
+
 int product(Node * head){
     int p = 1;
     Node * temp = head ;
@@ -46,6 +59,25 @@ int product(Node * head){
         temp = temp -> next;
     }
     return p ;
+}
+
+//insert at end
+void insertAtend(Node* head, int val){
+    Node* t = new Node(val);
+    while(head->next != NULL){
+        head = head ->next ;
+    }
+    head->next = t;
+}
+
+void insertAtHead(int val){
+    Node* temp = new Node(val);
+    if(size==0) head = tail= temp;
+    else{
+        temp->next = head;
+        head = temp ;
+    }
+    size++ ;
 }
 int main(){
     Node* a = new Node(10);
@@ -57,10 +89,59 @@ int main(){
     b->next = c;
     c->next = d;
     d->next = e;
-    display(a);
+    display(a) ;
+    cout << endl ;
+    displayRec(a);
+    cout << endl;
+    displayReverse(a);
     cout << endl;
     cout << size(a) << endl;
     cout << sum(a) << endl;
     cout << product(a) << endl;
+    cout << endl;
+    insertAtend(a,80);  // inserting last of linked list
+    display(a) ;
+    insertAtHead(a,50);
+    cout << endl;
+    display(a) ;
 }
 
+//for revision 
+// write function  to dispaly , size , add, product 
+
+// void display(Node* head){
+//     Node *temp = head ;
+//     while(temp != NULL){
+//         cout << temp -> val << " ";
+//         temp = temp -> next;
+//     }
+// }
+
+// int size(Node* head){
+//     Node* temp = head;
+//     int n = 0;
+//     while(temp != NULL){
+//         n++ ;
+//         temp = temp -> next ;
+//     }
+//     return n;
+// }
+
+// int (Node* head){
+//     // Node* temp = head ;
+//     int a = 0;
+//     while(head != NULL){
+//         a = a + head->val;
+//         head = head ->next ;
+//     }
+//     return a ;
+// }
+
+// int product(Node *head){
+//     int p = 1;
+//     while(head != NULL){
+//         p = p * head->val ;
+//         head = head ->next;
+//     }
+//     return p;
+// }
---
+++ LinkedList/Lecture1/linkedlistMethod.cpp
@@ -0,0 +1,8 @@
+//Linkedlis Method => 
+
+// linkedlis ll;
+// ll.add(20);
+// ll.addAthead(70);
+// ll.delete(2);
+// ll.insertAt(3,40);
+
