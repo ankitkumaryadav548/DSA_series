72fdd77338739f8c36f93bdc03ba4c6f03528c5f
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-09-18 13:51:49 +0530

    stact revision

--- Stack/Lecture1/revisionOfStack.cpp
+++ Stack/Lecture1/revisionOfStack.cpp
@@ -3,22 +3,36 @@
 using namespace std ;
 int main(){
     stack<int>st;
-    cout << st.size() << endl; //0
+    // cout << st.size() << endl; //0
     st.push(10);
     st.push(20);
     st.push(30);
     st.push(40);
     st.push(50);
-    // cout << st.size()<< endl;  // 5
+    cout << st.size()<< endl;  // 5
     st.pop();
     cout << st.top();  // 40
     st.push(100);  
     // How to traverse stack
 
+    // while(st.size()>0){
+    //  cout << st.top() << endl;  // 40 30 20 10
+    //  st.pop();   
+    // }
+    //how to getback element  after printing / popping
+    // we will male extra temp stack
+    stack<int>temp;
     while(st.size()>0){
-     cout << st.top() << endl;  // 40 30 20 10
-     st.pop();   
+        cout << st.top() << endl;
+        temp.push(st.top()) ;
+        st.pop();
+    } 
+    while(temp.size()>0){
+     st.push(temp.top());
+     temp.pop() ;  
     }
+    cout << endl<< st.top();
+
     
 }
 
