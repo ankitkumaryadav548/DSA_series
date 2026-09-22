08b5ed3f34cbb83da4dd09514827e9cc6e0d4ec4
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-09-22 11:24:37 +0530

    revision of previous greater element and stock span problem

--- Stack/Lecture2/previouGreaterElement.cpp
+++ Stack/Lecture2/previouGreaterElement.cpp
@@ -1,29 +1,60 @@
-//find greater previous element from the array
+// //find greater previous element from the array
+
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
+//     int arr[] = {3,1,2,5,4,6,2,3};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<n;i++){
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     int pge[n];
+//     pge[0] = -1;
+//     // cout << pge[0];
+//     stack<int>st;
+//     st.push(arr[0]);
+//     for(int i=1;i<n;i++){
+//         while(st.size()>0 && st.top()<=arr[i]) st.pop();  //pop
+//         if(st.size()==0) pge[i] = -1;
+//         else pge[i] = st.top();  //store ans
+//         st.push(arr[i]);  //push
+//     }
+
+//     for(int i=0;i<n;i++){
+//         cout << pge[i] << " ";
+//     }
+
+// }
+
+//practise => previous greater element
 
 #include<iostream>
 #include<stack>
 using namespace std;
 int main(){
-    int arr[] = {3,1,2,5,4,6,2,3};
+    int arr[] = {3,5,7,1,3,7,9,6};
     int n = sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n;i++){
         cout << arr[i] << " ";
     }
     cout << endl;
+
+    stack<int>st;
     int pge[n];
     pge[0] = -1;
-    // cout << pge[0];
-    stack<int>st;
     st.push(arr[0]);
     for(int i=1;i<n;i++){
         while(st.size()>0 && st.top()<=arr[i]) st.pop();  //pop
-        if(st.size()==0) pge[i] = -1;
+        if(st.size()==0) pge[i] = -1;    
         else pge[i] = st.top();  //store ans
-        st.push(arr[i]);  //push
+        st.push(arr[i]); //push
     }
-
     for(int i=0;i<n;i++){
         cout << pge[i] << " ";
     }
 
+
 }
+
--- Stack/Lecture2/stockSpan.cpp
+++ Stack/Lecture2/stockSpan.cpp
@@ -1,4 +1,34 @@
 
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
+//     int arr[] = {100,80,60,81,70,60,75,85};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<n;i++){
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     int pgi[n];
+//     pgi[0] = 1;
+//     stack<int>st;
+//     st.push(0);
+//     for(int i=1;i<n;i++){
+//         while(st.size()>0 && arr[st.top()]<=arr[i]) st.pop();  //pop
+//         if(st.size()==0) pgi[i] = -1;
+//         else pgi[i] = st.top();  //store ans
+//         pgi[i] = i - pgi[i]; // difference of indexes 
+//         st.push(i);  //push
+//     }
+
+//     for(int i=0;i<n;i++){
+//         cout << pgi[i] << " ";
+//     }
+
+// }
+
+//practise of stock span problem
+
 #include<iostream>
 #include<stack>
 using namespace std;
@@ -14,15 +44,14 @@ int main(){
     stack<int>st;
     st.push(0);
     for(int i=1;i<n;i++){
-        while(st.size()>0 && arr[st.top()]<=arr[i]) st.pop();  //pop
-        if(st.size()==0) pgi[i] = -1;
-        else pgi[i] = st.top();  //store ans
-        pgi[i] = i - pgi[i]; // difference of indexes 
-        st.push(i);  //push
+        while(st.size()>0 && arr[st.top()]<= arr[i]) st.pop();
+        if(st.size() == 0) pgi[i] = -1;
+        else pgi[i] = st.top();
+        pgi[i] = i - pgi[i];
+        st.push(i);
     }
-
     for(int i=0;i<n;i++){
-        cout << pgi[i] << " ";
+        cout << pgi[i] <<" ";
     }
-
+    
 }
